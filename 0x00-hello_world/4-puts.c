6382b67bfb0709a0456fefb6eee1c6b26fac8f6c
weiraw <weirawa@gmail.com>
2022-04-07 13:37:52 -0700

    fix task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
+ *main - entry point
  *
- *Return: Always 0 (Success)
+ *Return: 0 after printing the function
  */
 int main(void)
 {
-puts("\"programming is like building a multilingual puzzle");
-
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
