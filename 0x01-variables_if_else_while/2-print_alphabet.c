25ab508a59f4aa69e83250bd4657de6184d71e2e
weiraw <weirawa@gmail.com>
2022-04-09 09:40:14 -0700

    Task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,11 +7,12 @@
  */
 int main(void)
 {
-char c;
-
-for (c = 'a'; c <= 'z'; c++)
+	char c;
+	
+	for (c = 'a'; c <= 'z'; c++)
          putchar(c);
+	
+	putchar("\n");
+	return (0);
 
-putchar("\n");
-return (0);
 }
