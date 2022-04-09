9c2172abb2affea05ecea1b4703b7c44f81bcbbf
weiraw <weirawa@gmail.com>
2022-04-09 13:50:33 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 /**
  * main - Entry point
  *  
@@ -6,5 +8,5 @@
 int main(void)
 {
 printf("with proper grammer, but the outcome is a piece of art,\n");
-	    return (0);
+return (0);
 }
