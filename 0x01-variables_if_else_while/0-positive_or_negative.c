1e2bc45b192af08bdbc1abfd07491aee569b5af3
weiraw <weirawa@gmail.com>
2022-04-09 00:45:07 -0700

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,16 +11,16 @@ int main(void)
 {
 int n;
 
-strand(time(0));
+srand(time(0));
 n = rand() - RAND_MAX / 2;
 
-if (n > 0)
-printf("%d is positive\n", ,)
-else if (n == 0)
-printf(%d is zero\n", n);
+if (n > 1)
+printf("%d is positive\n", n)
+else if (n < 0)
+printf("%d is negative\n", n);
 else
 
-printf("%d is negative\n" , n);
+printf("%d is zero\n" , n);
 
 return (0);
 }
