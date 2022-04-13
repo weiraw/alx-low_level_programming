ec5fd5688d19f671aadf5a0acceb7567c4ecbc74
weiraw <weirawa@gmail.com>
2022-04-13 10:46:30 +0200

    my commit message

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,27 +1,24 @@
 #include <stdio.h>
+
 /**
- * main - causes an infinite loop
-
-[A * 
- * Return: 0
- */
-int main(void)
-{
-int i;
-
-printf("Infinite loop incoming :(\n);
-
-i = 0;
-
-/*
- *while (i < 10)
- *{
-[A[A[A[A[A[A[A[A[A[A[A[A[A[C[A[C[C[C[B[B[B[B[B[B[B[B[B[B[B[B[B[C
- *Infinite loop - No increase of variable
- *putchar(i);
-}
+* main - causes an infinite loop
+* Return: 0
 */
 
-printf("Infinte loop avoided! \\0/\n");
-return (0);
-}
+int main(void)
+{
+        int i;
+
+        printf("Infinite loop incoming :(\n");
+
+        i = 0;
+
+        while (i < 10)
+        {
+                putchar(i);
+        }
+
+        printf("Infinite loop avoided! \\o/\n");
+
+        return (0);
+}[A[A[A[C[C[C[C[C[A[A[A[C[C[A[A[A[A[A[A[A[A[B[B[B[B/*[C[C[C[[C[C[C[C[C[C[C[C[C[whi[D[D[Dw[D[D*[B[D*[B[B[A[D*[B[D[C[B[C[D[D*/[A[*/[C[C[C[C[C[C[A[B[B[B[B[B[B[D[D[D[D[D
