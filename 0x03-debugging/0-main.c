24148b9934fe41313e78077e0add7cfab573e1c2
weiraw <weirawa@gmail.com>
2022-04-14 21:40:06 +0200

    task 1

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,16 @@
 #include "main.h"
+
 /**
- * main - main
- * Return: 0
+ * main - tests function that prints if integer is positive or negative
+ * Return: zero
  */
 
 int main(void)
 {
 int i;
-i = 0;
 
+i = 0;
 positive_or_negative(i);
 
 return (0);
-
 }
--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,18 +7,18 @@
 
 int main(void)
 {
-        int i;
+int i;
 
-        printf("Infinite loop incoming :(\n");
+printf("Infinite loop incoming :(\n");
 
-        i = 0;
+i = 0;
 
-        while (i < 10)
-        {
-                putchar(i);
-        }
-
-        printf("Infinite loop avoided! \\o/\n");
-
-        return (0);
-}[A[A[A[C[C[C[C[C[A[A[A[C[C[A[A[A[A[A[A[A[A[B[B[B[B/*[C[C[C[[C[C[C[C[C[C[C[C[C[whi[D[D[Dw[D[D*[B[D*[B[B[A[D*[B[D[C[B[C[D[D*/[A[*/[C[C[C[C[C[C[A[B[B[B[B[B[B[D[D[D[D[D
+   /*
+    * while (i < 10)
+    * {
+    * putchar(i);
+    * }
+    */
+printf("Infinite loop avoided! \\o/\n");
+return (0);
+}
