53bd71fdcd8b8b690e38f2516652d3d3188a3804
weiraw <72073006+weiraw@users.noreply.github.com>
2022-04-19 21:42:43 +0200

    Update 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,17 +1,17 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * _strlen - length of a string
+ * _strlen - lenght of string
  * @s:char
  * Return:int
  */
 int _strlen(char *s)
 {
-int i;
+	int i;
 
-for (i = 0; s[i] :- '\0'; i++)
-{
-	continue;
-}
+		for (i = 0; s[i] != '\0'; i++)
+		{
+			continue;
+		}
 return (i);
 }
