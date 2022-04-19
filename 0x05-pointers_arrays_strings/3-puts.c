a74b621315e80d70d6a7993b5e5ed76fa06236d4
weiraw <weirawa@gmail.com>
2022-04-19 13:39:14 +0200

    my second task commit

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,12 +1,1 @@
-#include "main.h"
-/**
-*_puts - prints a string
-*@str: A pointer to an int that will be changed
-*
-*Return: void which means our answer is correct
-*/
-
-void _puts(char *str)
-{
-char *c;
-int Z[?64;1;2;4;6;9;15;21;22;28;29c`[C[zZ[?64;1;2;4;6;9;15;21;22;28;29cZ[?64;1;2;4;6;9;15;21;22;28;2Z[?64;1;2;4;6;9;15;21;22;28;29c
+[A[A[A[B[B[B[B
