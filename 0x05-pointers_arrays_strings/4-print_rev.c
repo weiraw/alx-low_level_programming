50787ec5a22d4c1bc5531089f45cd62e57f41538
weiraw <72073006+weiraw@users.noreply.github.com>
2022-04-19 21:50:27 +0200

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,24 +1,23 @@
-/**
- * print_rev - prints a string in reverse order
- * 0s: A pointer to an int that will be changed
- *
- *Return: void which means our answer is correct
- */
+#include "holberton.h"
 
+/**
+ * print_rev - print a string
+ * @s:pointer char
+ * return:void
+ */
 void print_rev(char *s)
 {
-int i;
+	int i, n;
 
-i = 0;
-while (s[i] != '\0')
-{
-i++;
-}
-
-for (i = i - 1 ; i >= 0; i--)
-{
-_putchar (s[i]);
-}
-
-_putchar ('\n');
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		continue;
+	}
+	n = i - 1;
+	for (i = n; i >= 0 ; i--)
+	{
+		_putchar(s[i]);
+	}
+_putchar('\n');
+return;
 }
